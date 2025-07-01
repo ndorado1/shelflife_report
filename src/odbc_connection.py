61e52961e579772b6ea8e395f4a597c44599c501
Dorado Guarin <ndorado1@its.jnj.com>
2025-07-01 10:08:04 -0500

    Extract Data from Terra

--- src/odbc_connection.py
+++ src/odbc_connection.py
@@ -2,6 +2,7 @@
 
 import pyodbc
 import pandas as pd
+import os
 
 # Connection details
 dsn = "DenodoODBCTerraProd"
@@ -10,7 +11,13 @@ user = "ndorado1@its.jnj.com"
 password = "Comercio2024"
 
 # Example SQL query - replace with your table or view
-sql_query = "SELECT TOP 10 * FROM view_ra_action_license_details"
+sql_query = """
+SELECT * FROM view_ra_action_license_details
+WHERE "Region" = 'LATAM'
+"""
+output_path="ODBC/data/raw/LATAM_ra_action_licenses_details.csv"
+
+os.makedirs("C:\Users\NDorado1\OneDrive - JNJ\Documentos\ODBC\data\raw", exist_ok=True)
 
 # Create connection string using DSN
 conn_str = f"DSN={dsn};DATABASE={database};UID={user};PWD={password};"
@@ -23,7 +30,10 @@ try:
 
     # Run query
     df = pd.read_sql(sql_query, conn)
-    print(df.head())
+    print(f"Total Rows: {len(df)}")
+
+    df.to_csv (output_path,index=False)
+    print(f"CSV saved to {output_path}")
 
     conn.close()
 
